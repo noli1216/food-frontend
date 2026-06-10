65c0791dcc8b110e77ccbdc7dbc3fe5fb985f177
kurabachew Mengistu <kmengistu1216@gmail.com>
2026-06-10 15:56:54 -0500

    updated

--- src/pages/AdminMenu.jsx
+++ src/pages/AdminMenu.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from '../config/api';
-import "./adminmenu.css";
+import "./adminMenu.css";
 
 function AdminMenu() {
   const [menus, setMenus] = useState([]);
